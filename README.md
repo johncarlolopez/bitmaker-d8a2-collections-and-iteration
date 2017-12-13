c1674ed5fdc81d14c1270008fc20c47c5e1e3fa9
John Carlo Lopez <condottiere.jc@gmail.com>
2017-12-13 17:24:49 -0500

    complete exercise 9 and update readme

--- README.md
+++ README.md
@@ -149,31 +149,34 @@ After you're done, be sure you have everything committed and pushed to your gith
 
 ### Exercise 10
 ___
-Create a file named exercise9.rb.
+Create a file named **exercise9.rb**.
 
 Don't forget to try running your code after each step, and to commit frequently as you work!
 
-Start out by creating the following hash representing the number of students in past Bitmaker cohorts:
-
+1. Start out by creating the following hash representing the number of students in past Bitmaker cohorts:
+```
 students = {
   :cohort1 => 34,
   :cohort2 => 42,
   :cohort3 => 22
 }
-Create a method that displays the name and number of students for each cohort, like so:
+```
+2. Create a method that displays the name and number of students for each cohort, like so:
+```
+cohort1: 34 students
+cohort2: 42 students
+cohort3: 22 students
+```
+3. Add cohort 4, which had 43 students, to the hash.
 
+4. Use the ```keys``` method to output all of the cohort names.
 
+5. The classrooms have been expanded! Increase each cohort size by 5% and display the new results.
 
-Add cohort 4, which had 43 students, to the hash.
+6. Delete the 2nd cohort and redisplay the hash.
 
-Use the keys method to output all of the cohort names.
+7. BONUS: Calculate the total number of students across all cohorts using ```each``` and a variable to keep track of the total. Output the result.
 
-The classrooms have been expanded! Increase each cohort size by 5% and display the new results.
-
-Delete the 2nd cohort and redisplay the hash.
-
-BONUS: Calculate the total number of students across all cohorts using each and a variable to keep track of the total. Output the result.
-
-BONUS: Create another similar hash called staff and display it using the display method from above.
+8. BONUS: Create another similar hash called ```staff``` and ```display``` it using the display method from above.
 
 After you're done, be sure you have committed and pushed everything to your Github repo.
--- exercise.rb
+++ exercise.rb
@@ -205,3 +205,42 @@ puts "The sum is: $#{sum(expenses)}"
 puts "The sum is: $#{sum(expensestwo)}"
 
 #Exercise 9
+# We're going to make a shopping list by storing a few items in an array. Feel free to start with whatever items you like:
+# ```
+grocery_list = ["carrots", "toilet paper", "apples", "salmon"]
+# ```
+# After each step, run your program to ensure it works before you move onto the next one. It's a good idea to commit often, too.
+#
+# 1. Your next step should present your grocery list with an item on each line, with an asterisk (\*) in front of it so that it appears like this:
+# ```
+# *carrots
+# *toilet paper
+# *apples
+# *salmon
+# ```
+grocery_list.each {|item| puts "* #{item}"}
+#
+# 2. You realize you've forgotten some rice, so add it to your list and output it again. Given that you've already output your list twice, it might be good to consider writing a method to do this. Putting frequently used chunks of code in a method lets you reuse them throughout your program without having to type them out over and over.
+grocery_list << "rice"
+def printer(array)
+  array.each {|item| puts "* #{item}"}
+end
+printer(grocery_list)
+# 3. You lost count of how many things you need to pick up. Better output the total number of items on your list.
+puts "There are #{grocery_list.count} items in your list"
+# 4. Check to see if your list includes "bananas". If it does, output "You need to pick up bananas". Otherwise, output "You don't need to pick up bananas today".
+# grocery_list << "bananas"
+if (grocery_list.select { |item| item.to_s.downcase == "bananas"  }).count >= 1
+  puts "You need to pick up bananas"
+else
+  puts "You don't need to pick up bananas today"
+end
+# 5. Display the second item in the list. (Don't forget that arrays indices start at zero!)
+puts grocery_list[1]
+# 6. It turns out that everything in this grocery store you're visiting is stored alphabetically, so to better plan out what you need to buy you should sort your grocery list and output it with asterisks again.
+grocery_list.sort!
+printer(grocery_list)
+# 7. After looking everywhere, you can't find the salmon. Delete it from your list and redisplay the list one last time.
+grocery_list.delete("salmon")
+printer(grocery_list)
+# After you're done, be sure you have everything committed and pushed to your github repo.
