974165ae0fd97e1a910d867cdb7b3c63262cfcbb
John Carlo Lopez <condottiere.jc@gmail.com>
2017-12-13 15:57:53 -0500

    complete exercise 6 and update readme

--- README.md
+++ README.md
@@ -99,18 +99,13 @@ ___
 
 ### Exercise 7
 ___
-Output the message "I will not skateboard in the halls" 20 times.
-
-Create an array consisting of the above message. It should appear in the array 20 times.
-
-Create an array consisting of the numbers between 1 and 50.
-
-Use an each loop to find the sum of the numbers in the above array.
-
-Create a new array which has three of each number up to 50.
-
-Ie. [1, 1, 1, 2, 2, 2, 3, 3, 3, ... , 50, 50, 50] and so on, up to 50.
-Make a new array out all of the countries from the hash in Exercise 6 that are not islands. Print out both arrays.
+1. Output the message "I will not skateboard in the halls" 20 times.
+2. Create an array consisting of the above message. It should appear in the array 20 times.
+3. Create an array consisting of the numbers between 1 and 50.
+4. Use an each loop to find the sum of the numbers in the above array.
+5. Create a new array which has three of each number up to 50.
+  * Ie. ```[1, 1, 1, 2, 2, 2, 3, 3, 3, ... , 50, 50, 50]``` and so on, up to 50.
+6. Make a new array out all of the countries from the hash in Exercise 6 that are not islands. Print out both arrays.
 
 
 ### Exercise 8
--- exercise.rb
+++ exercise.rb
@@ -131,3 +131,35 @@ my_colours.insert(-1,"purple","yellow")
 #adds to the end but also allows to insert multiple
 
 # Exercise 6
+# 1. Make a new hash that contains a list of movies for each year. Each list of movies should be an array. Below is some data you can use.
+#   * 1999: The Matrix, Star Wars: Episode 1, The Mummy
+#   * 2009: Avatar, Star Trek, District 9
+#   * 2019: How to Train Your Dragon 3, Toy Story 4, Star Wars: Episode 9
+movies = {
+  1999 => ["The Matrix", "Star Wars: Episode 1", "The Mummy"],
+  2009 => ["Avatar", "Star Trek", "District 9"],
+  2019 => ["How to Train Your Dragon 3", "Toy Story 4", "Star Wars: Episode 9"]
+}
+# 2. Make a new array that contains each row of the buttons on a phone. Each row should be an array.
+#   * The rows on a phone are: ```1 2 3```, ```4 5 6```, ```7 8 9```, ```* 0 #```
+phone = [[1,2,3],[4,5,6],[7,8,9],['*',0,'#']]
+# 3. Make a new array that contains information about three countries. Each country should be a hash that has a name, a continent, and whether or not it is an island.
+countries = [
+  {
+    name: "Canada",
+    continent: "North America",
+    is_island: false
+  },
+  {
+    name: "South Africa",
+    continent: "Africa",
+    is_island: false
+  },
+  {
+    name: "United Kingdom",
+    continent: "Europe",
+    is_island: true
+  }
+]
+
+# Exercise 7
